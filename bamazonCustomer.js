50206ab6e3aa512c23b9c1e2f52770ada4635d45
tonygnc1 <bootcamp1955@gmail.com>
2019-08-08 09:52:55 -0400

    Stock-quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -54,56 +54,29 @@ function queryAllProducts() {
                 },
             ]).then(function (answers) {
                 console.log(answers);
-                connection.query("SELECT * FROM products", function (err, res) {
-                    // console.log(res);
-                    if (err) throw err;
-                    for (var i = 0; i < res.length; i++) {
-                    }
-                    var stock_quantity = stock_quantity - parseInt(answers.howMany)
+                connection.query("SELECT * FROM products WHERE ?", [{ id: answers.whatId
+                    }], function (err, res) {
+                        if (err) throw err;
+console.log(res);
+                        if (res[0].stock_quantity >= parseInt(answers.howMany)) {
+                            connection.query("UPDATE products SET  ? WHERE ?", [{ stock_quantity: res[0].stock_quantity - parseInt(answers.howMany) }, { id: parseInt(answers.whatId) }], function (err, res) {
+                                if (err) throw err
+                                else {
+                                    queryAllProducts()
+                                }
+                            })
+                        } else {
+                            console.log("Insufficient Quantity")
+                        }
+                        // connection.end();
 
-
-                    var j
-                    for (var i = 0; i < res.length; i++) {
-                        if (answers.whatId == res[i].itemId) j = i
-                    }
-                    console.log(j);
-                    if ((res[j].stock_quantity >= parseInt(answers.howMany)) && (res[j].itemId == answers.whatId)) {
-                        connection.query("UPDATE products SET  stock_quantity ="  + "WHERE itemId =" + answers.whatId, function (err, res) {
-                            console.log("UPDATE products SET  stock_quantity =1" + stock_quantity - parseInt(answers.howMany) + "WHERE itemId =" + answers.whatId);
-                            // console.log(res.affectedRows + " record(s) updated");
-                        })
-                    } else {
-                        console.log("Insufficient Quantity")
-
-                    }
-
-
-                })
+                    })
                 // console.log(res);
-                // connection.end();
             })
     });
-
-
-    // connection.end();
-
 };
 
-
-
-
-
-
-
-// logs the actual query being run
-// console.log(query.sql);
 // connection.end();
 
 
 
-
-//     .then(function() {
-//         if   {
-// .howMany === res[i].stock_quantity
-//         }
-//     })
